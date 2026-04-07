285bd424e92f182f80c2412ba16adb603ccb5ad2
vortex3964 <theo2005.adams@gmail.com>
2026-04-07 18:26:30 +0300

    frontend : updated depedencies for livekit

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -3,8 +3,11 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  connectivity_plus
   file_selector_windows
   flutter_secure_storage_windows
+  flutter_webrtc
+  livekit_client
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
